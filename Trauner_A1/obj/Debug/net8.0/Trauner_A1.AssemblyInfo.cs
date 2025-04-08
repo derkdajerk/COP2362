b657d78cc0eac2795fb9ad5928d5f48fe72b2015
derkdajerk <derektrauner@gmail.com>
2025-04-07 18:27:44 -0700

    Project Info

--- Trauner_A1/obj/Debug/net8.0/Trauner_A1.AssemblyInfo.cs
+++ Trauner_A1/obj/Debug/net8.0/Trauner_A1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trauner_A1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d44c77b0df5f45f9ae0e4903101bef817f15164e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfad8d68ab97ea83f3c358330a4547e2d5189a4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trauner_A1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trauner_A1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
